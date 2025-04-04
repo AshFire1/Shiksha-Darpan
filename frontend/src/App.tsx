ee71d160cc1c8e5e9f5f0288244d926301e69928
AshFire1 <112924336+AshFire1@users.noreply.github.com>
2025-04-04 17:43:39 +0530

    added home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,14 @@ import Student from './pages/Student.tsx';
 import Company from './pages/Company.tsx';
 import Login from './pages/Login.tsx';
 import Register from './pages/Register.tsx';
+import Home from './pages/Home.tsx';
 
 function App() {
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/" element={<h1><Home/></h1>} />
         <Route path="/student" element={<Student />} />
         <Route path="/company" element={<Company />} />
         <Route path="/login" element={<Login />} />
