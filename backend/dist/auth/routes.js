fdb4bf60e82918354dd30ecab966c659f339a917
AshFire1 <112924336+AshFire1@users.noreply.github.com>
2025-04-06 13:14:35 +0530

    Delete backend/dist directory

--- backend/dist/auth/routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const mainAuth_1 = require("./mainAuth");
-const authRouter = (0, express_1.Router)();
-authRouter.post("/signup", mainAuth_1.signUp);
-authRouter.post("/login", mainAuth_1.signIn);
-exports.default = authRouter;
